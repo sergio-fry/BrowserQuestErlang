41e36da8976848170da023a92356978c3a7463eb
DeTeam <me@deteam.ru>
2012-09-22 14:19:51 +0400

    Working stuff

--- src/bq_app.erl
+++ src/bq_app.erl
@@ -10,6 +10,27 @@
 %% ===================================================================
 
 start(_StartType, _StartArgs) ->
+  Dispatch = [
+      {'_', [
+        {[<<"client">>, '...'], cowboy_http_static, [
+          {directory, <<"node/client">>},
+          {mimetypes, {fun mimetypes:path_to_mimes/2, default}}
+        ]},
+        {[<<"shared">>, '...'], cowboy_http_static, [
+          {directory, <<"node/shared">>},
+          {mimetypes, {fun mimetypes:path_to_mimes/2, default}}
+        ]}
+        % ,
+        % {[<<"proxy">>, '...'], bq_proxy, [{upstream, "http://localhost:8001/"}]},
+        % {'_', bq_client, []}
+      ]}
+  ],
+
+  cowboy:start_listener(bq_http_server, 10,
+      cowboy_tcp_transport, [{port, 8000}],
+      cowboy_http_protocol, [{dispatch, Dispatch}]
+  ),
+  
     bq_sup:start_link().
 
 stop(_State) ->
